46e1d048b7e2c6ea612f9deddfc3f8d279d8e88b
walterbm <walter.beller.morales@gmail.com>
2015-09-19 22:54:46 -0400

    refactor using js object

--- js/main.js
+++ js/main.js
@@ -1,44 +1,18 @@
-//Create a text database
-var db = new SQL.Database();
-// Run a query without reading the results
-db.run("CREATE TABLE test (firstName, lastName);");
-// Seed Database
-db.run("INSERT INTO test VALUES (?,?)", ["Hello","World"]);
-db.run("INSERT INTO test VALUES (?,?)", ["Eve", "Jackson"]);
-db.run("INSERT INTO test VALUES (?,?)", ["John", "Doe"]);
-db.run("INSERT INTO test VALUES (?,?)", ["Adam","Johnson"]);
-db.run("INSERT INTO test VALUES (?,?)", ["Jill", "Smith"]);
+// Using SqlSandbox wrapper
+var db = new SqlSandbox();
 
-// // Prepare a SQL statement for execution
-var statement = $("#commands").text();
-var executed = db.prepare(statement);
-executed.getAsObject();
+// Create database
+db.runCommand("CREATE TABLE test (firstName, lastName);");
 
-// Output all database rows
-while(executed.step()) {
-    var row = executed.getAsObject();
-    var rowElement = "<tr><td>"+row.firstName+"</td><td>"+row.lastName+"</td></tr>";
-    $('#results').append(rowElement);
-}
+// Seed database
+db.runCommand("INSERT INTO test VALUES (?,?)", ["Hello","World"]);
+db.runCommand("INSERT INTO test VALUES (?,?)", ["Eve", "Jackson"]);
+db.runCommand("INSERT INTO test VALUES (?,?)", ["John", "Doe"]);
+db.runCommand("INSERT INTO test VALUES (?,?)", ["Adam","Johnson"]);
+db.runCommand("INSERT INTO test VALUES (?,?)", ["Jill", "Smith"]);
 
-// Load from local SQLlite DB
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'sql/SCOTUS.sqlite', true);
-xhr.responseType = 'arraybuffer';
-
-xhr.send();
-
-xhr.onload = function(e) {
-  var uInt8Array = new Uint8Array(this.response);
-  var db = new SQL.Database(uInt8Array);
-  var contents = db.prepare("SELECT * FROM SCOTUS");
-  contents.getAsObject();
-  while(contents.step()) {
-    var row = contents.getAsObject();
-    var rowElement = "<tr><td>"+row.justiceName+"</td><td>"+row.post_mn+"</td></tr>";
-    $('#results').append(rowElement);
-  }
-};
+// Execute command and collect result
+var executed = db.executeCommand("SELECT * FROM test");
 
 
 
---
+++ js/sqlSandbox.js
@@ -0,0 +1,37 @@
+function SqlSandbox(){
+  this.db = new SQL.Database();
+}
+
+SqlSandbox.prototype.loadDb = function(database_url, callback){
+  var self = this;
+  this.ajaxRequest(database_url, function(){
+    var uInt8Array = new Uint8Array(this.response);
+    self.db = new SQL.Database(uInt8Array);
+    callback();
+  });
+};
+
+SqlSandbox.prototype.ajaxRequest = function(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'arraybuffer';
+  xhr.send();
+  xhr.onload = callback;
+};
+
+SqlSandbox.prototype.executeCommand = function(commandString){
+  return this.db.exec(commandString);
+};
+
+SqlSandbox.prototype.runCommand = function(commandString){
+  this.db.run(commandString);
+};
+
+SqlSandbox.prototype.runCommands = function(commandString){
+  var self = this;
+  commandArray = commandString.split(';');
+  commandArray.forEach(function(command){
+    self.runCommand(command);
+  });
+};
+
